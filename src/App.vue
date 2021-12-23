cf975a04c4e64e593b78b6d574bc7836da1fd7f9
lovct <362605832@qq.com>
2021-12-23 15:37:37 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>Hello Vue 2 + Vite</div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-view></router-view>
+  </div>
+</template>
